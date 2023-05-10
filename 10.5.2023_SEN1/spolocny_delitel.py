f88e8f6a56f49e757d4970d4d1cb19dc9843b1e2
Tomáš Lovrant <66744640+tomuisgod@users.noreply.github.com>
2023-05-10 13:53:49 +0200

    Update and rename idk.py to spolocny_delitel.py

--- 10.5.2023_SEN1/spolocny_delitel.py
+++ 10.5.2023_SEN1/spolocny_delitel.py
@@ -1,11 +1,11 @@
-def blbost(a: int, b: int):
+def sd(a: int, b: int):
     while a != b:
         if a < b:
             a,b = b,a
         a = a - b
     return b
 
-print(blbost(173, 53))
+print(sd(173, 53))
 
 
 
